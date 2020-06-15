8dbe29347d3d11d322b5aa3162cd81ccf4c6a5bb
juan-cantero <juan.cantero@outlook.com>
2020-06-15 16:26:03 -0300

    add remove function

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // @ts-nocheck
 
+function* IdGenerator() {
+  let id = 0;
+  while(true) 
+    yield id += 1
+  
+}
+const id = IdGenerator();
+
+
 class BookStore {
   constructor() {
     this.form = document.querySelector('.form')
     this.table = document.querySelector('.book-table tbody')
     this.books = []
     this.render()
-    this.onSubmit()
+    this.addBookListeners()
     
   }
 
@@ -19,18 +28,35 @@ class BookStore {
     return  document.querySelector('.form__author-input').value
 
   }
-  onSubmit() {
-    this.form.addEventListener('submit',(e)=> {
-      e.preventDefault()
-      const book = {
+  addBookListeners() {
+    this.form.addEventListener('submit',this.addBook)
+  }
+
+  removeBookListeners = () => {
+    const books = document.querySelectorAll(".remove");
+    console.log(books)
+    books.forEach(b => b.addEventListener('click',this.removeBook))
+  }
+
+  removeBook = e => {
+    const targetId = e.target.id
+    const index = this.books.findIndex(b => b.id.toString() ===targetId)
+    this.books.splice(index,1)
+    this.render()
+  }
+
+  addBook = e =>  {
+    e.preventDefault()
+    const book = {
+        id:id.next().value,
         title:this.title,
         author:this.author
       }
       this.books.push(book)
-      console.log(this.books)
       this.render()
-    })
+      this.removeBookListeners()
   }
+  
 
   render() {
     let tableData=''
@@ -40,11 +66,13 @@ class BookStore {
          `<tr>
             <td>${b.title}</td>
             <td>${b.author}</td>
+            <td class="remove" id="${b.id}">X</td>
           </tr>
           `
     })
 
     this.table.innerHTML = tableData
+    this.removeBookListeners()
   }
 }
 
